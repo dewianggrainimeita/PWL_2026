1c34ba333a52dd2987c9b999f352716ccc93230a
Dewi Anggraini Meita Sari <ssitiaisya2@gmail.com>
2026-02-28 00:10:36 +0700

    Update Jobsheet 02 Routing Controller View

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function About(){
+        return 'NIM : 244107020068, NAMA : Dewi Anggraini Meita Sari';
+    }
+}
